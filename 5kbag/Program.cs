272fb022ca808007af38b977e821213ea8a224e6
flbas1 <flbas1@gmail.com>
2017-02-16 15:19:04 -0500

    needed more code

--- 5kbag/Program.cs
+++ 5kbag/Program.cs
@@ -1,33 +1,22 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _5kbag
 {
     class Program
     {
-        public   const int totalMemory= 4;
-        public static string memory = new string(' ', 1024 * totalMemory);
+       
+        #region "defaults for project.  This area should be ok"
+        /// <summary>
+        /// how much memory we are able to use
+        /// </summary>
+        public const int totalMemory = 4;
 
-
-        static void Main(string[] args)
-        {
-            var list = getList(1024 * (totalMemory+1));  //5k list of numbers
-
-            foreach (var item in list)
-            {
-
-                memory.Add(memoryLocation, item);
-                if (memory.Length > )
-            }
-
-
-        }
-
-
+        /// <summary>
+        /// our actual memory container
+        /// </summary>
+        public static string memory = new string(' ', 1024 * totalMemory);  //you can change to another container if necessary
 
         /// <summary>
         /// generates a random list of numbers, range from 0 to 65535.  This code is ok; shouldn't need to be fixed.
@@ -47,6 +36,35 @@ namespace _5kbag
                 retVal.Add(randomNumber);
             }
             return retVal;
+        }
+
+        #endregion
+
+        static void Main(string[] args)
+        {
+            var list = getList(1024 * (totalMemory + 1));  //5k list of numbers
+
+            foreach (var item in list)
+            {
+
+                #region "Your task is to make this block work.  make the Poke and Peek methods"
+                int memoryLocation = 0;  // this is the actual memory location we are going to update
+
+                memory.Poke(memoryLocation, item);  //we are going to add the item to the memory container.  you are going to have to write Poke
+                #endregion
+
+
+                if (memory.Length > 1024 * totalMemory)
+                    throw new OutOfMemoryException();
+            }
+
+            //done.  now lets test
+            for (var loc = 0; loc < list.Count - 1; loc++)
+                if (list[loc] != memory.Peek[loc])  //please make this line work also
+                    throw new NotFiniteNumberException();
+            Console.WriteLine("Success!");
+
+
         }
     }
 }
