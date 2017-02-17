92d6f7b9ccbf691447437e0062a69a5721aeff48
HelderSepu <heldersepu@gmail.com>
2017-02-16 22:20:11 -0500

    store the items in the string
    We know that the items are a list numbers in incremental order.
    I take advantage of that and substract the number and the predecessor,
    that way we only "store" one char per position, and of course we
    compress the final string.

--- 5kbag/StringExtensions.cs
+++ 5kbag/StringExtensions.cs
@@ -2,14 +2,23 @@
 {
     public static class StringExtensions
     {
-        public static void Poke(this string value, int loc, uint item)
+        public static string Poke(this string value, int loc, uint item)
         {
-            //
+            string tmp = value.Decompress();
+            int last = 0;
+            foreach (char c in tmp)
+                last += c - '0';
+            tmp += (item - last).ToString();
+            return tmp.Compress();
         }
 
         public static uint Peek(this string value, int loc)
         {
-            return 0;
+            string tmp = value.Decompress();
+            int num = 0;
+            for (int i = 0; i <= loc; i++)
+                num += tmp[i] - '0';
+            return (uint)num;
         }
     }
 }
